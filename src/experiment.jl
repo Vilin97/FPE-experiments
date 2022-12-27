aeb1f0784c8db57a903e8b1fde8bf0b57e90f858
Vasily Ilin <vasilin97@gmail.com>
2022-12-26 22:29:18 -0500

    changing the NN architecture for the first example

--- src/experiment.jl
+++ src/experiment.jl
@@ -6,6 +6,26 @@ include("sbtm.jl")
 include("jhu.jl")
 
 # first example from paper
+
+function potential(xs, u1, u2)
+    d_bar, N, n = size(xs)
+    term1 = reshape(sum(u1(xs), dims = 2), n) # first term of the potential in eqn (13)
+    # TODO write the second term
+end
+
+function initialize_s(ρ₀, xs, size_hidden, num_hidden; activation = relu, verbose = 1, kwargs...)
+    d_bar, N, n = size(xs)
+    s = Chain(
+        xs -> reshape(xs, d_bar*N,n)
+        Dense(d => size_hidden, activation),
+        repeat([Dense(size_hidden, size_hidden), activation], num_hidden-1)...,
+        Dense(size_hidden => d),
+        xs -> reshape(xs, d_bar, N, n))
+    epochs = initialize_s!(s, ρ₀, xs; kwargs...)
+    verbose > 0 && println("Took $epochs epochs to initialize. Initial loss: $(loss(s,xs))")
+    return s
+end
+
 function moving_trap_experiment()
     d_bar, N, n = 5, 10, 20
     seed = d_bar*N*n
@@ -22,14 +42,14 @@ function moving_trap_experiment()
 
     println("Done with jhu. Took $(t2-t1) seconds.")
 
-    s = initialize_s(ρ₀, xs, 32, 1)
+    s = initialize_s(ρ₀, xs, 100, 1)
     epochs = 25
     t3 = time()
     println("Done with NN initialization. Took $(t3-t2) seconds.")
     sbtm_trajectories, losses, s_values = sbtm(xs, Δts, b, D, s; epochs = epochs, record_losses = true, verbose = 0)
     t4 = time()
     # animation = animate_2d(trajectories, "sbtm", Δts, samples = 2, fps = 10, target = target)
-    # loss_plot = plot_losses(losses, epochs)
+    # loss_plot = plot_losses(losses)
 
     println("Done with sbtm. Took $(t4-t3) seconds.")
 
